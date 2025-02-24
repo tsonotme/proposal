b077b181fab60fbc11a00a303a0ed247e06ad30e
tsonotme <mumma.a.gurl.behind.you@gmail.com>
2025-02-24 14:34:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+const messages = [
+    "Are you sure?",
+    "Really sure??",
+    "Are you positive?",
+    "Nah, reconsider it",
+    "Just think about it!",
+    "Give it a second thought",
+    "I will be bored without you..",
+    "I will be very very very sad...",
+    "Ok fine, I will stop asking...",
+    "Just kidding, say yes please! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
